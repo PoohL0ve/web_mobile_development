0a1d95cd0cc7e051fa93cc1d448e9e308cdb21fb
PoohL0ve <screwed06@gmail.com>
2025-07-13 02:42:46 -0400

    Webpack essentials

---
+++ front-end/javascript/code_practice/webpack-practice/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+import "./styles.css";
+import greeting from "./greeting.js"; // Don't need the extension
+
+console.log(greeting);
---
+++ front-end/javascript/code_practice/webpack-practice/webpack.config.js
@@ -0,0 +1,31 @@
+"use strict";
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // cleans the output each time webpack is run
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+    },
+};

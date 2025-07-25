dac63921ff744ef7f3410f65f983312e97d47749
PoohL0ve <screwed06@gmail.com>
2025-07-25 03:38:08 -0400

    Creating String Permutations

---
+++ front-end/javascript/code_practice/permutations/script.js
@@ -0,0 +1,38 @@
+/** String Permutation
+ * All the characters of the string would be arranged
+ * in different locations. 
+ */
+// Use the pre Value to accumulate characters to form the permutation
+const permuteString = (word, pre='', container=[]) => {
+    // Base case
+    if (word.length === 0 ) {
+        container.push(pre);
+        return container;
+    } 
+    
+    let charArray = word.split('').sort();
+    let lastChar = '';
+    for (let i = 0; i < charArray.length; i++) {
+        let char = charArray[i];
+        
+        if (i > 0 && char === lastChar) {
+            continue;
+        }
+
+        let newPre = pre + char;
+        
+        console.log(` Prefix Value: ${newPre}`);
+        let remainingArray = [...charArray]; 
+        remainingArray.splice(i, 1);
+        let remainStr = remainingArray.join('');
+
+        lastChar = char;
+
+        permuteString(remainStr, newPre, container);
+    }
+    return container;
+}
+    
+    
+
+console.log(permuteString("fcc"));
